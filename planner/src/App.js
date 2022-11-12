218c7ab0a3e42a65621f5b2c1fcc4374e7df7483
Zorark47 <zjw5qs@virginia.edu>
2022-11-12 17:41:11 -0500

    task stuff

--- planner/src/App.js
+++ planner/src/App.js
@@ -8,7 +8,6 @@ import About from './pages/about';
 import Login from './pages/login';
 import Task from './pages/task';
 import Register from './pages/register';
-import Count from './pages/count';
   
 function App() {
 return (
@@ -18,7 +17,6 @@ return (
         <Route exact path='/' element={<Home />} />
         <Route path='/about' element={<About/>} />
         <Route path='/task' element={<Task/>} />
-        <Route path='/count' element={<Count/>} />
         <Route path = '/login' element ={<Login/>} />
         <Route path = '/register' element ={<Register/>} />
     </Routes>
