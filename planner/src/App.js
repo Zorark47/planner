3b194e655c99070c6c5d55c51d14c2cca1388aef
Zorark47 <zjw5qs@virginia.edu>
2022-11-11 20:57:16 -0500

    cleaning

--- planner/src/App.js
+++ planner/src/App.js
@@ -20,46 +20,14 @@ const handleClick2 = () => {
 }
 
 return (
-	<div style={{
-	display: 'flex',
-	flexDirection: 'column',
-	alignItems: 'center',
-	justifyContent: 'center',
-	fontSize: '300%',
-	position: 'absolute',
-	width: '100%',
-	height: '100%',
-	top: '-15%',
-	}}>
+	<div>
 	Counter App
-	<div style={{
-		fontSize: '120%',
-		position: 'relative',
-		top: '10vh',
-	}}>
+	<div>
 		{counter}
 	</div>
 	<div className="buttons">
-		<button style={{
-		fontSize: '60%',
-		position: 'relative',
-		top: '20vh',
-		marginRight: '5px',
-		backgroundColor: 'green',
-		borderRadius: '8%',
-		color: 'white',
-		}}
-		onClick={handleClick1}>Increment</button>
-		<button style={{
-		fontSize: '60%',
-		position: 'relative',
-		top: '20vh',
-		marginLeft: '5px',
-		backgroundColor: 'red',
-		borderRadius: '8%',
-		color: 'white',
-		}}
-		onClick={handleClick2}>Decrement</button>
+		<button onClick={handleClick1}>Increment</button>
+		<button	onClick={handleClick2}>Decrement</button>
 	</div>
 	</div>
 )
