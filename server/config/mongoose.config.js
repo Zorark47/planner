44bfebabccb539b0055d8f39645d506729a769c5
altisseront <altisseront@gmail.com>
2022-11-12 11:38:14 -0700

    1

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -3,9 +3,11 @@ const db_name = 'examdb';
 
 
 //mongoose connection
-mongoose.connect(`mongodb+srv://root:root@clustermay.t9fcf.mongodb.net/${db_name}?retryWrites=true&w=majority`, {
+mongoose.connect(`mongodb+srv://root:root@cluster0.ms1glhc.mongodb.net/?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
     .then(() => console.log('Established a connection to the database'))
     .catch(err => console.log('Something went wrong when connecting to the database ', err));
+
+    
